dc7a286d2def6184d6f435e593e4651e4f24bf6f
dsjsantos <dssantos@gmail.com>
2020-09-05 20:39:04 -0300

    Adicionado um teste com Mockito

--- src/test/java/local/djsantos/CalculadoraTest.java
+++ src/test/java/local/djsantos/CalculadoraTest.java
@@ -3,6 +3,8 @@ package local.djsantos;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class CalculadoraTest {
 
@@ -11,6 +13,18 @@ public class CalculadoraTest {
         Calculadora calculadora = new Calculadora();
         int soma = calculadora.somar("1 + 5 + 3");
         assertEquals(9, soma);
+    }
+
+    @Test
+    public void testSomarWithMock() {
+        Calculadora calculadora = mock(Calculadora.class);
+
+        when(calculadora.somar("1+3")).thenReturn(10);
+
+        int resultado = calculadora.somar("1+3");
+
+        assertEquals(10, resultado);
+
     }
 
 }
